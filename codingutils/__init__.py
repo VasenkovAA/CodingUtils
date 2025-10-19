09f98e0ce05568a9ffff901c54b99c932cb9d13e
Andrey <andrej.500@mail.ru>
2025-10-19 16:09:33 +0300

    feat: add pyproject

---
+++ codingutils/__init__.py
@@ -0,0 +1,11 @@
+"""
+CodingUtils - A comprehensive set of Python utilities for code analysis, 
+file management, and project documentation.
+"""
+
+__version__ = "1.0.0"
+__author__ = "CodingUtils Team"
+
+from .comment_extractor import main as comment_extractor_main
+from .merger import main as merger_main
+from .tree_generater import main as tree_generater_main
